8a3ab2cfcc825a30624f70fcefdca30966d1a418
Eric Puentes <eric.david2333@gmail.com>
2023-02-06 08:02:09 -0500

    build macro: simplify maps

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -9,18 +9,59 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let bs_name = format_ident!("{}Builder", input_name);
     let input_data = match input.data {
         Data::Struct(ds) => ds,
-        _ => panic!("Only works in named structs"),
+        _ => unreachable!("Only works in named structs"),
     };
-    let bs_attr: Vec<_> = input_data
+    let indents: Vec<_> = input_data
         .fields
-        .iter()
+        .into_iter()
         .map(|f| {
-            let f_name = format_ident!("{}", f.ident.as_ref().unwrap());
-            let f_type = match &f.ty {
+            let f_name = format_ident!("{}", f.ident.unwrap());
+            let f_type = match f.ty {
                 syn::Type::Path(v) => v,
-                _ => panic!("Only works in named structs"),
+                _ => unreachable!("Only works in named structs"),
             };
+            (f_name, f_type)
+        })
+        .collect();
+    let bs_attr: Vec<_> = indents
+        .iter()
+        .map(|(f_name, f_type)| {
             quote! { #f_name: Option<#f_type> }
+        })
+        .collect();
+    let bf_attr: Vec<_> = indents
+        .iter()
+        .map(|(f_name, _)| {
+            quote! { #f_name: None }
+        })
+        .collect();
+    let funcs: Vec<_> = indents
+        .iter()
+        .map(|(f_name, f_type)| {
+            quote! {
+                pub fn #f_name(&mut self, #f_name: #f_type) -> &mut Self {
+                    self.#f_name = Some(#f_name);
+                    self
+                }
+            }
+        })
+        .collect();
+    let build: Vec<_> = indents.iter()
+        .map(|(f_name, _)| {
+            let f_string = format!("{f_name}");
+            quote! {
+                if(self.#f_name.is_none()) {
+                    return Err(format!("None value in {}\n you need all attributes set", #f_string).into());
+                }
+            }
+        })
+        .collect();
+    let command_f: Vec<_> = indents
+        .iter()
+        .map(|(f_name, _)| {
+            quote! {
+                #f_name: self.#f_name.clone().unwrap()
+            }
         })
         .collect();
     let builder_struct = quote! {
@@ -28,15 +69,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
             #(#bs_attr),*
         }
     };
-
-    let bf_attr: Vec<_> = input_data
-        .fields
-        .iter()
-        .map(|f| {
-            let f_name = format_ident!("{}", f.ident.as_ref().unwrap());
-            quote! { #f_name: None }
-        })
-        .collect();
 
     let impl_struct = quote! {
         impl #input_name {
@@ -47,50 +79,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         }
     };
-
-    let funcs: Vec<_> = input_data
-        .fields
-        .iter()
-        .map(|f| {
-            let f_name = format_ident!("{}", f.ident.as_ref().unwrap());
-            let f_type = match &f.ty {
-                syn::Type::Path(v) => v,
-                _ => panic!("Only works in named structs"),
-            };
-            quote! {
-                pub fn #f_name(&mut self, #f_name: #f_type) -> &mut Self {
-                    self.#f_name = Some(#f_name);
-                    self
-                }
-            }
-        })
-        .collect();
-
-    let build: Vec<_> = input_data
-        .fields
-        .iter()
-        .map(|f| {
-            let f_ident = f.ident.as_ref().unwrap();
-            let f_name = format_ident!("{}", f_ident);
-            let f_string = format!("{f_ident}");
-            quote! {
-                if(self.#f_name.is_none()) {
-                    return Err(format!("None value in {}\n you need all attributes set", #f_string).into());
-                }
-            }
-        })
-        .collect();
-
-    let command_f: Vec<_> = input_data
-        .fields
-        .iter()
-        .map(|f| {
-            let f_name = format_ident!("{}", f.ident.as_ref().unwrap());
-            quote! {
-                #f_name: self.#f_name.clone().unwrap()
-            }
-        })
-        .collect();
 
     let impl_builder = quote! {
         impl #bs_name {
