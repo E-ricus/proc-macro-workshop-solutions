dd51bba48a63cb827fc252ee5c82239a64f05495
Eric Puentes <eric.david2333@gmail.com>
2023-02-06 08:05:12 -0500

    build macro: finish macro

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
+use proc_macro2::Ident;
 use quote::{format_ident, quote};
-use syn::{Data, DeriveInput};
+use syn::{Data, DeriveInput, Meta, NestedMeta};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as DeriveInput);
     let input_name = input.ident;
@@ -11,57 +12,101 @@ pub fn derive(input: TokenStream) -> TokenStream {
         Data::Struct(ds) => ds,
         _ => unreachable!("Only works in named structs"),
     };
-    let indents: Vec<_> = input_data
+    let idents: Vec<_> = input_data
         .fields
         .into_iter()
         .map(|f| {
-            let f_name = format_ident!("{}", f.ident.unwrap());
-            let f_type = match f.ty {
+            let name = format_ident!("{}", f.ident.unwrap());
+            // Asummes only one attribute
+            let mut attribute: Option<Ident> = None;
+            if let Some(attr) = f.attrs.first() {
+                if let Ok(Meta::List(list)) = attr.parse_meta() {
+                    if let Some(NestedMeta::Meta(Meta::NameValue(value))) = list.nested.first() {
+                        if let Some(path) = value.path.segments.first() {
+                            let ident = &path.ident;
+                            if (format!("{ident}") != "each") {
+                                unreachable!(
+                                    "Invalid name for attribute, the correct value is each"
+                                )
+                                // Investigate how to span the compile error
+                                // compile_error!(
+                                //     "Invalid name for attribute, the correct value is each"
+                                // );
+                            }
+                        }
+                        if let syn::Lit::Str(str) = value.lit.to_owned() {
+                            let at = Ident::new(&str.value(), str.span());
+                            attribute = Some(at);
+                        }
+                    }
+                }
+            }
+            let ty = match f.ty {
                 syn::Type::Path(v) => v,
                 _ => unreachable!("Only works in named structs"),
             };
-            (f_name, f_type)
+            (name, ty, attribute)
         })
         .collect();
-    let bs_attr: Vec<_> = indents
+    let bs_attr: Vec<_> = idents
         .iter()
-        .map(|(f_name, f_type)| {
-            quote! { #f_name: Option<#f_type> }
+        .map(|(name, ty, _)| match inner_type(ty, "Option") {
+            (true, ty) => quote! { #name: std::option::Option<#ty> },
+            (false, ty) => quote! { #name: std::option::Option<#ty> },
         })
         .collect();
-    let bf_attr: Vec<_> = indents
+    let bf_attr: Vec<_> = idents
         .iter()
-        .map(|(f_name, _)| {
-            quote! { #f_name: None }
+        .map(|(name, _, attr)| match attr {
+            Some(_) => quote! { #name: std::option::Option::Some(std::vec::Vec::new()) },
+            None => quote! { #name: std::option::Option::None },
         })
         .collect();
-    let funcs: Vec<_> = indents
+    let funcs: Vec<_> = idents
         .iter()
-        .map(|(f_name, f_type)| {
-            quote! {
-                pub fn #f_name(&mut self, #f_name: #f_type) -> &mut Self {
-                    self.#f_name = Some(#f_name);
-                    self
+        .map(|(name, ty, attr)| {
+            let (_, ty) = inner_type(ty, "Option");
+            match attr {
+                Some(str) => {
+                    let (_, ty) = inner_type(ty, "Vec");
+                    quote! {
+                        pub fn #str(&mut self, #str: #ty) -> &mut Self {
+                            if(self.#name.is_none()){
+                            }
+                            self.#name.as_mut().unwrap().push(#str);
+                            self
+                        }
+                    }
+                }
+                None => {
+                    quote! {
+                        pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                            self.#name = std::option::Option::Some(#name);
+                            self
+                        }
+                    }
                 }
             }
         })
         .collect();
-    let build: Vec<_> = indents.iter()
-        .map(|(f_name, _)| {
-            let f_string = format!("{f_name}");
+    let build: Vec<_> = idents.iter().filter(|(_, ty, attr)| {
+            let (ok, _) = inner_type(ty, "Option");
+            !ok && attr.is_none()
+        })
+        .map(|(name, _, _)| {
+            let f_name = format!("{name}");
             quote! {
-                if(self.#f_name.is_none()) {
-                    return Err(format!("None value in {}\n you need all attributes set", #f_string).into());
+                if(self.#name.is_none()) {
+                    return std::result::Result::Err(format!("None value in {}\n All not Optional attributes must be set", #f_name).into());
                 }
             }
         })
         .collect();
-    let command_f: Vec<_> = indents
+    let command_f: Vec<_> = idents
         .iter()
-        .map(|(f_name, _)| {
-            quote! {
-                #f_name: self.#f_name.clone().unwrap()
-            }
+        .map(|(name, ty, _)| match inner_type(ty, "Option") {
+            (true, _) => quote! { #name: self.#name.clone() },
+            (false, _) => quote! { #name: self.#name.clone().unwrap() },
         })
         .collect();
     let builder_struct = quote! {
@@ -83,9 +128,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let impl_builder = quote! {
         impl #bs_name {
             #(#funcs)*
-            pub fn build(&mut self) -> Result<#input_name, Box<dyn std::error::Error>> {
+            pub fn build(&mut self) -> std::result::Result<#input_name, std::boxed::Box<dyn std::error::Error>> {
                 #(#build)*
-                Ok(
+                std::result::Result::Ok(
                     #input_name {
                         #(#command_f),*
                     }
@@ -101,4 +146,19 @@ pub fn derive(input: TokenStream) -> TokenStream {
         #impl_builder
     };
     TokenStream::from(expanded)
+}
+
+fn inner_type<'ty>(ty: &'ty syn::TypePath, outter: &str) -> (bool, &'ty syn::TypePath) {
+    if let Some(syn::PathSegment { ident, arguments }) = ty.path.segments.first() {
+        // Assumes Option to be written like so
+        if &*ident.to_string() == outter {
+            if let syn::PathArguments::AngleBracketed(ab) = arguments {
+                if let Some(syn::GenericArgument::Type(syn::Type::Path(path))) = ab.args.first() {
+                    return (true, path);
+                }
+            }
+            return (true, ty);
+        }
+    }
+    (false, ty)
 }
